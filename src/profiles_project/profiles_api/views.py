e8133a71890edc380ded55fdf920f276141aa5ac
solaleh81 <solalehalizadeh81.com>
2024-04-25 22:20:33 +0330

    add: HelloApiView class.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """
+    Test Api View.
+    """
+
+    def get(self, request, format=None):
+        """
+        Returns a list of ApiView features.
+        """
+        an_apiview = [
+            'test',
+            'test2'
+        ]
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+    
