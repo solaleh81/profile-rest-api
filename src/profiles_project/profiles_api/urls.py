795b3fd0373c20a2841cdae8c3070827dbe6cc91
solaleh81 <solalehalizadeh81.com>
2024-04-26 16:25:23 +0330

    add: profile_feed url.

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -7,7 +7,7 @@ router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet, basename='userprofile')
 router.register('login', views.LoginViewSet, basename='login')
-
+router.register('profile-feed', views.UserProfileFeedViewSet, basename='profile_feed')
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view(), name='test'),
     path('', include(router.urls)),
